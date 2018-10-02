044b910870f99141bda42f2adb5a56390142c176
Whisky <623384675@qq.com>
2018-10-02 16:40:45 +0700

    第一次提交

---
+++ js/provins.js
@@ -0,0 +1,111 @@
+function pyChinese(pro){
+	var str = '';
+	switch(pro){
+		case '黑龙江':
+			str = 'heilongjiang';
+			break;
+		case '河北':
+			str = 'hebei';
+			break;
+		case '甘肃':
+			str = 'gansu';
+			break;
+		case '云南':
+			str = 'yunnan';
+			break;
+		case '四川':
+			str = 'sichuan';
+			break;
+		case '吉林':
+			str = 'jilin';
+			break;
+		case '辽宁':
+			str = 'liaoning';
+			break;
+		case '青海':
+			str = 'qinghai';
+			break;
+		case '陕西':
+			str = 'shanxi';
+			break;
+		case '河南':
+			str = 'henan';
+			break;
+		case '山东':
+			str = 'shandong';
+			break;
+		case '山西':
+			str = 'shanxi';
+			break;
+		case '安徽':
+			str = 'anhui';
+			break;
+		case '湖北':
+			str = 'hubei';
+			break;
+		case '湖南':
+			str = 'hunan';
+			break;
+		case '江苏':
+			str = 'jiangsu';
+			break;
+		case '贵州':
+			str = 'guizhou';
+			break;
+		case '浙江':
+			str = 'zhejiang';
+			break;
+		case '江西':
+			str = 'jiangxi';
+			break;
+		case '广东':
+			str = 'guangdong';
+			break;
+		case '福建':
+			str = 'fujian';
+			break;
+		case '台湾':
+			str = 'taiwan';
+			break;
+		case '海南':
+			str = 'hainan';
+			break;
+		case '广西':
+			str = 'guangxi';
+			break;
+		case '内蒙古':
+			str = 'neimenggu';
+			break;
+		case '宁夏':
+			str = 'ningxia';
+			break;
+		case '新疆':
+			str = 'xinjiang';
+			break;
+		case '西藏':
+			str = 'xizang';
+			break;
+		case '澳门':
+			str = 'aomen';
+			break;
+		case '北京':
+			str = 'beijing';
+			break;
+		case '上海':
+			str = 'shanghai';
+			break;
+		case '香港':
+			str = 'xianggang';
+			break;
+		case '天津':
+			str = 'tianjin';
+			break;
+		case '重庆':
+			str = 'chongqing';
+			break;
+		default: 
+			str = '';
+			break;
+	}
+	return str;
+}
